390c56b3579f78160eae6041783e3037c357c2b4
UnopposedQuill <UnopposedQuill@github.com>
2019-08-24 00:43:30 -0600

    Agregadas las especificaciones, información del fuente de la población por cantón, así como implementado un uso básico de los 3 archivos principales.

--- contenido.js
+++ contenido.js
@@ -1,4 +1,13 @@
 
 //Este archivo define cómo se muestra el contenido del gráfico.
-//Para poder usar D3 aquí
-var d3 = require("d3");
+//Si no coloco la importación de d3 en el html debo colocar esto aquí
+//para poder usar D3 aquí
+//var d3 = require("d3");
+
+//Tengo que colocar las variables del Xlsx suministrado en el repositorio aquí:
+var poblaciones = [];
+var areas = [];
+var densidades = [];
+
+//Ahora colocar las dimensiones específicas del svg, que es donde dibujaré todo
+//SVG significa 
