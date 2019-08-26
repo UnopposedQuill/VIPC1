d25dba841e5f0ceda2261e66c8cfbe8cfdbf1c5a
UnopposedQuill <UnopposedQuill@github.com>
2019-08-26 02:15:40 -0600

    Se agregaron funciones dummy para representar los cambios de dibujos en el gráfico

--- funciones.js
+++ funciones.js
@@ -7,6 +7,26 @@ function max(number1, number2) {
     return number2;
 }
 
-function burbujas() {
-
+//Esta función lo que hará es redibujar el gráfico según los valores de las variables
+function redibujar() {
+    console.log("Redibujando");
 }
+
+//Cambia el tipo de dibujo a burbujas, y luego llama a redibujar el gráfico
+function burbujas() {
+    console.log("Burbujas");
+    redibujar();
+}
+
+//Cambia el tipo de dibujo a rectángulos, y luego llama a redibujar el gráfico
+function rectangulos() {
+    console.log("Rectángulos");
+    redibujar();
+}
+
+//Cambia el tipo de dibujo a glifos, y luego llama a redibujar el gráfico
+function glifos() {
+    console.log("Glifos");
+    redibujar();
+}
+
