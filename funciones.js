79345740c006197ddea5e63b04baf7d127c97f18
UnopposedQuill <UnopposedQuill@github.com>
2019-08-26 08:17:00 -0600

    Agregado un .gitignore para quitar los archivos de VS

--- funciones.js
+++ funciones.js
@@ -1,7 +1,7 @@
 
 //Algunas funciones auxiliares que necesité
 function max(number1, number2) {
-    if (number1 > 1) {
+    if (number1 > number2 {
         return number1;
     }
     return number2;
