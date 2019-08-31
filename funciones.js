bbe1970f147b336dc78d41e92ca8250b2237fe70
UnopposedQuill <UnopposedQuill@github.com>
2019-08-31 15:35:36 -0600

    Implementados los rectángulos, diamantes, y círculos; así como el cambio entre figuras, usando el nuevo motor de figuras

--- funciones.js
+++ funciones.js
@@ -7,94 +7,6 @@ function max(number1, number2) {
     return number2;
 }
 
-//Esta función lo que hará es redibujar el gráfico según los valores de las variables
-function redibujar() {
-    console.log("Redibujando");
-    if (tipoDibujo === "burbujas") {
-        svg.selectAll("circle")
-            .data(cantones)
-            .enter()
-            .append("circle")
-            .attr("cx", function (d) {
-                return xScale(d.poblacion);
-            })
-            .attr("cy", function (d) {
-                return yScale(d.area) - 20;
-            })
-            .attr("r", function (d) {
-                return radioBase * radiusScale(d.densidad);
-            })
-            .attr("class", "figure")
-            //Ahora que ya todo está en su lugar, agrego
-            //los tooltips a cada una de las figuras colocadas
-            .on("mouseover", tip.show)
-            .on("mouseout", tip.hide);
-    } else if (tipoDibujo === "rectangulos") {
-        svg.selectAll("rect")
-            .data(cantones)
-            .enter()
-            .append("rect")
-            .attr("x", function (d) {
-                return xScale(d.poblacion) - 5;
-            })
-            .attr("y", function (d) {
-                return yScale(d.area) - 25;
-            })
-            .attr("width", function (d) {
-                return 10;
-            })
-            .attr("height", function (d) {
-                return 5;
-            })
-            .attr("class", "figure")
-            .on("mouseover", tip.show)
-            .on("mouseout", tip.hide);
-    } else if (tipoDibujo === "glifo") {
-
-    } else {
-        console.log("Tipo incorrecto de dibujo");
-    }
-}
-
-//Cambia el tipo de dibujo a burbujas, y luego llama a redibujar el gráfico
-function burbujas() {
-    console.log("Burbujas");
-    tipoDibujo = "burbujas";
-    svg.selectAll("circle").transition().attr("visibility", "visible");
-    svg.selectAll("rect").transition().attr("visibility", "hidden");
-    svg.selectAll("diamond").transition().attr("visibility", "hidden");
-    //redibujar();
-}
-
-//Cambia el tipo de dibujo a rectángulos, y luego llama a redibujar el gráfico
-function rectangulos() {
-    console.log("Rectángulos");
-    tipoDibujo = "rectangulos";
-    svg.selectAll("circle").transition().attr("visibility", "hidden");
-    svg.selectAll("rect").transition().attr("visibility", "visible");
-    svg.selectAll("diamond").transition().attr("visibility", "hidden");
-    //redibujar();
-}
-
-//Cambia el tipo de dibujo a burbujas, y luego llama a redibujar el gráfico
-function diamantes() {
-    console.log("Diamond");
-    tipoDibujo = "Diamond";
-    
-    svg.selectAll("circle").transition().attr("visibility", "hidden");
-    svg.selectAll("rect").transition().attr("visibility", "hidden");
-    svg.selectAll("diamond").transition().attr("visibility", "visible");
-    //redibujar();
-}
-
-//Cambia el tipo de dibujo a glifos, y luego llama a redibujar el gráfico
-function glifos() {
-    console.log("Glifos");
-    tipoDibujo = "glifos";
-    redibujar();
-}
-
-
 function cambiarColor() {
     //Busca el color dentro del componente CambiadorColor y se lo aplica
     //los objetos dentro de la grafica
@@ -141,34 +53,6 @@ function cambioTamanno() {
 
 }
 
-function mostradorBurbujas() {
-    tipoDibujo = document.getElementById("figuraBurbujas").value;
-    console.log(tipoDibujo);
-
-    svg.selectAll("circle").transition().attr("visibility", "visible");
-    svg.selectAll("rect").transition().attr("visibility", "hidden");
-    svg.selectAll("diamond").transition().attr("visibility", "hidden");
-}
-
-
-function mostradorRectangulos() {
-    tipoDibujo = document.getElementById("figuraRectangulos").value;
-    console.log(tipoDibujo);
-
-    svg.selectAll("rect").transition().attr("visibility", "visible");
-    svg.selectAll("circle").transition().attr("visibility", "hidden");
-    svg.selectAll("diamond").transition().attr("visibility", "hidden");
-}
-
-function mostradorDiamantes() {
-    tipoDibujo = document.getElementById("figuraDiamantes").value;
-    console.log(tipoDibujo);
-
-    svg.selectAll("rect").transition().attr("visibility", "hidden");
-    svg.selectAll("circle").transition().attr("visibility", "hidden");
-    svg.selectAll("diamond").transition().attr("visibility", "visible");
-}
-
 function mostrarGraficoBarras(){
 
     // Primero oculta a figura que se esta mostrando
@@ -194,8 +78,15 @@ function mostrarGraficoBarras(){
 
 }
 
+//Esta función se llama al cambiar alguno de los botones de radios
 function cambiarFigura(self) {
-    symbol.type(self.id);
+    if (self.id === "figuraBurbujas") {
+        symbol.type(d3.symbolCircle);
+    } else if (self.id === "figuraRectangulos") {
+        symbol.type(rectangulo);
+    } else if (self.id === "figuraDiamantes") {
+        symbol.type(d3.symbolDiamond);
+    }
     path.attr('d', symbol);
 }
 
